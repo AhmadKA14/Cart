9b86f86009a93ccbcc0b80a29342865a68ef6ad4
AhmadAbushawar <ahmad.abushawar@ontariotechu.net>
2022-10-22 00:11:35 -0400

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useGlobalContext } from './components/context'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+
+
+export default function App() {
+    const { loading } = useGlobalContext()
+
+    if (loading) {
+        return (
+            <div className="loading">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    return (
+        <main>
+            <Navbar />
+            <CartContainer />
+        </main>
+    )
 }
-
-export default App;
